4abd142a2ec8e59d74bc160670c0898b08c0cafd
vit1204 <nguyenthanhviet12042004@gmail.com>
2023-04-17 11:06:22 +0700

    update DAY4 s-group

--- databases/connection.js
+++ databases/connection.js
@@ -1,16 +1,20 @@
-console.log(process.env.DB_HOST);
 
-//  const mysql2 = require('mysql2')
-// let connections = mysql2.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: '1204',
-//   database: 'nodejs'
-// });
+ const mysql2 = require('mysql2')
+import dotenv from 'dotenv'
+dotenv.config();
 
-// connections.connect((error) => {
-//   if(error){throw error};
-//   console.log('Connected');
-// } )
 
-// module.exports = connections;
+let connections = mysql2.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DataBase,
+  
+});
+
+connections.connect((error) => {
+  if(error){throw error};
+  console.log('Connected');
+} )
+
+module.exports = connections;
